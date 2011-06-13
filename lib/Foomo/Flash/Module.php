939573b9fdeb5742c91057faf6bf5b32850ab8c1
Jan Halfar <jan@bestbytes.de>
2011-06-13 21:03:42 +0200

    module clean up

--- lib/Foomo/Flash/Module.php
+++ lib/Foomo/Flash/Module.php
@@ -29,7 +29,6 @@ class Module extends ModuleBase {
 			\Foomo\Flex\Settings::$FLEX_HOME = $flexConfig->home;
 		}
 	}
-
 	/**
 	 * Get a plain text description of what this module does
 	 *
@@ -39,37 +38,4 @@ class Module extends ModuleBase {
 	{
 		return 'Adobe Flash (tm)(r) Integration';
 	}
-
-	/**
-	 * get a translation object for a resource in your module
-	 *
-	 * @param string $resourceName name of the resource will point to /path/to/moduleRoot/locale/<locale>/<resourceName>.yml
-	 * @param string[] $localeChain ordered preferences for your translations like array('en_US', 'de_DE, ...) or array('en', 'de') or ...
-	 *
-	 * @return \Foomo\Translation
-	 */
-	public static function getTranslation($resourceName, $localeChain = null)
-	{
-		return \Foomo\Translation::getModuleTranslation(self::NAME, $resourceName, $localeChain);
-	}
-
-	/**
-	 * get all the module resources
-	 *
-	 * @return \Foomo\Modules\Resource[]
-	 */
-	public static function getResources()
-	{
-		return array(
-			// get a run mode independent folder var/<runMode>/test
-			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'test'),
-			// and a file in it
-			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_File, 'test' . DIRECTORY_SEPARATOR . 'someFile'),
-			// request a cache resource
-			// \Foomo\Modules\Resource\Fs::getCacheResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'navigationLeaves'),
-			// a database configuration
-			// \Foomo\Modules\Resource\Config::getResource(self::NAME, 'Foomo.Flash.flex')
-		);
-	}
-
 }
